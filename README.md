07313f560609defe9ab79e53257df1f9e1961381
kairulla <kayrulla@bk.ru>
2022-10-01 23:29:55 +0600

    completed

---
+++ README.md
@@ -0,0 +1,5 @@
+<img src="_MY_PICTURES/TASK_4_transparencyRED.png" width=100% align="middle"><img src="_MY_PICTURES/2022-10-01_230751.png" width=50% align="middle"><img src="_MY_PICTURES/2022-10-01_230817.png" width=50% align="middle">
+<img src="_MY_PICTURES/2022-10-01_230825.png" width=50% align="middle"><img src="_MY_PICTURES/2022-10-01_230834.png" width=50% align="middle">
+<img src="_MY_PICTURES/2022-10-01_230858.png" width=50% align="middle"><img src="_MY_PICTURES/2022-10-01_230919.png" width=50% align="middle">
+<img src="_MY_PICTURES/2022-10-01_231030.png" width=100% align="middle">
+<img src="_MY_PICTURES/logo.png" width=100% align="middle">
---
+++ main.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import sys
+
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QDialog, QApplication, QTableWidgetItem
+from PyQt5.uic import loadUi
+import math
+import random
+
+
+class Launcher(QDialog):
+    array_global = []
+    max_value = 0
+    max_i = 0
+
+    def __init__(self):
+        super(Launcher, self).__init__()
+        loadUi('g.ui', self)
+
+        self.setWindowTitle('Лабораторная 4 _ Python3 + PyQt5')
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
+
+        # Задание иконки окна
+        self.setWindowIcon(QtGui.QIcon('_MY_PICTURES//logo.png'))
+
+        self.qPushButtonRandom.clicked.connect(self.qPushButtonRandomOnClick)
+        self.qPushButtonSolver.clicked.connect(self.qPushButtonSolverOnClick)
+
+
+    def qPushButtonRandomOnClick(self):
+        n = 4
+        m = 5
+        max_number = 100
+
+        array = []
+        for i in range(0, n):
+            sub_array = []
+            for j in range(m):
+                number = random.randint(0, max_number)
+                sub_array.append(number)
+            array.append(sub_array)
+
+        for row, i in enumerate(array):
+            for col, j in enumerate(i):
+                self.qTableWidget.setItem(row, col, QTableWidgetItem(str(j)))
+
+        i = 0
+        max_value = array[1][i]
+        max_i = i
+        for i, j in enumerate(array[1]):
+            # print("i=", i)
+            if j > max_value:
+                max_value = j
+                max_i = i
+        self.qLabelMaxItemValue.setText("Максимальный элемент = %s" % max_value)
+        print("max_value=", max_value)
+        print("max_i=", max_i)
+        self.array_global = array.copy()
+        self.max_value = max_value
+        self.max_i = max_i
+
+
+
+
+    def qPushButtonSolverOnClick(self):
+        array = self.array_global.copy()
+        max_value = self.max_value
+        max_i = self.max_i
+        if (max_value > array[2][0]):
+            temp = array[2][0]
+            array[2][0] = max_value
+            array[1][max_i] = temp
+
+            for row, i in enumerate(array):
+                for col, j in enumerate(i):
+                    self.qTableWidget.setItem(row, col, QTableWidgetItem(str(j)))
+
+
+if __name__ == '__main__':
+    # Основная часть программы
+    app = QApplication(sys.argv)
+    window = Launcher()  # базовый класс окна
+    window.show()  # отобразить окно на экране
+    sys.exit(app.exec_())  # запуск основного цикла приложения
